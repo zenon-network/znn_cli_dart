2354f4836abd6f7049129375a879dd86b769aa41
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2023-12-12 13:03:10 +0100

    Use confirm function

--- lib/utils/inputUtils.dart
+++ lib/utils/inputUtils.dart
@@ -9,19 +9,3 @@ String? enterPassphrase() {
   stdin.echoMode = true;
   return passphrase;
 }
-
-bool retryOrAbort() {
-  try {
-    print("Press [${green('R')}]etry or [${red('A')}]bort to continue.");
-    stdin.echoMode = false;
-    stdin.lineMode = false;
-    while (true) {
-      var input = stdin.readByteSync();
-      if (input == 82 || input == 114) return true; // Retry
-      if (input == 65 || input == 97) return false; // Abort
-    }
-  } finally {
-    stdin.lineMode = true;
-    stdin.echoMode = true;
-  }
-}
